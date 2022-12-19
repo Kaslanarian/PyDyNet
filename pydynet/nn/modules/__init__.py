d74807fa3e2aeef443d3221f85b13bd6394136df
Welt Xing <62822155+Kaslanarian@users.noreply.github.com>
2022-12-19 23:33:00 +0800

    Update __init__.py
    modified a bug, thanks to @duma-repo

--- pydynet/nn/modules/__init__.py
+++ pydynet/nn/modules/__init__.py
@@ -1,6 +1,7 @@
 from .activation import Sigmoid, Tanh, ReLU, LeakyReLU, Softmax
 from .batchnorm import BatchNorm1d, BatchNorm2d
-from .conv import Conv1d, Conv2d, MaxPool1d, MaxPool2d, AvgPool1d, AvgPool2d
+from .conv import Conv1d, Conv2d
+from .pool import MaxPool1d, MaxPool2d, AvgPool1d, AvgPool2d
 from .dropout import Dropout
 from .linear import Linear
 from .loss import MSELoss, NLLLoss, CrossEntropyLoss
